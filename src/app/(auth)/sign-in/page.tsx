bfd59182d221dd8d9666828ffc5c5e6b4df05fe6
Vasu Agarwal <agarwal.vasu32@gmail.com>
2025-09-16 15:37:58 +0530

    fix: remove unnecessary console log from dbConnect function

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -2,7 +2,7 @@
 import { useSession, signIn, signOut } from "next-auth/react"
 
 export default function Component() {
-  const { data: session } = useSession()
+  const { data: session } = useSession()//this is a hook to take data from session
   if (session) {
     return (
       <>
--- src/lib/dbConnect.ts
+++ src/lib/dbConnect.ts
@@ -17,7 +17,6 @@ async function dbConnect(): Promise<void> {//void is telling i am not expecting 
         const db = await mongoose.connect(process.env.
             MONGODB_URI || '', {})
 
-            console.log(db)
             connection.isConnected = db.connections[0].readyState
 
             console.log("db connected successfully")
