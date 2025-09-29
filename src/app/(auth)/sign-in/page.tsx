dcf194290679e00ab3a58a297d21ccf2eeebded4
Vasu Agarwal <agarwal.vasu32@gmail.com>
2025-09-29 18:51:26 +0530

    feat: implement Input component and enhance sign-in page with form handling and username validation

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -10,7 +10,10 @@ import { useRouter } from "next/navigation"
 import { signUpSchema } from "@/schemas/signUpSchema"
 import axios, { AxiosError } from 'axios'
 import { ApiResponse } from "@/types/ApiResponse"
-// we have used debounce dusername so that we dont have to make call 
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+
+// we have used debounce username so that we dont have to make call 
 // to api on every click so we are using debouncing to wait some time and then make api calls
 
 const page = () => {
@@ -54,9 +57,55 @@ const page = () => {
     }
     checkUsernameUnique()
   }, [debouncedUsername])
+
+  const onSubmit = async (data: z.infer<typeof signUpSchema>) => {
+    setIsSubmitting(true)
+    console.log(data)
+    try {
+      const response = await axios.post<ApiResponse>('/api/sign-up', data)
+      toast("Success", { description: response.data.message })
+      router.replace(`/verify/${username}`)
+      setIsSubmitting(false)
+    } catch (error) {
+      console.error("Error in signup of user", error)
+      const axiosError = error as AxiosError<ApiResponse>
+      let errorMessage = axiosError.response?.data.message
+      toast( "Signup failed", { description: errorMessage, })
+      setIsSubmitting(false)
+    }
+  }
+
   return ( 
-    <div>
-      
+    <div className="flex justify-center items-center min-h-screen bg-gray-100">
+      <div className="w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow-md">
+        <div className="text-center">
+          <h1 className=" text-4xl font-extrabold tracking-tight lg:text-5xl mb-6">
+            Join Mystery Message
+            </h1>
+            <p className="mb-4" >Sign up to start your anonymous journey</p>
+        </div>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+            <FormField
+          name="username"
+          control={form.control}
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input placeholder="Username" {...field} 
+                onChange={(e) => {
+                  field.onChange(e)
+                  setUsername(e.target.value)
+                }} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+          </form>
+        </Form>
+      </div>
     </div>
   )
 }
